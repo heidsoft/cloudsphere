76ac803963bebfc063dce186a37a20fbc2fdcd1a
heidsoft <heidsoft@sina.com>
2017-03-06 17:53:18 +0800

    Update 初始化cmp平台

---
+++ src/main/java/com/heidsoft/NgcloudApplication.java
@@ -0,0 +1,13 @@
+package com.heidsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgcloudApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NgcloudApplication.class, args);
+	}
+}
