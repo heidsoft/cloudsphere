7db3b1999697c66a14c968cdeca5119890736ad3
heidsoft <heidsoft@sina.com>
2017-03-20 00:00:30 +0800

    Update 增加组件

--- web/src/app/tree.component.ts
+++ web/src/app/tree.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
   template: `
-    <h2>CRISIS CENTER</h2>
-    <p>Get your crisis here</p>`
+    <h2>路由测试</h2>
+    <p>route-let</p>
+`
 })
 export class TreeListComponent { }
---
+++ web/src/app/vm/app-vm.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "app-vm",
+  templateUrl: "./app-vm.component.html",
+})
+export class AppVmComponent  {
+
+}
+
+
+
+
+
