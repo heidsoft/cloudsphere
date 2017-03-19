24e04722833020817b504ca0e9f7eb57ac235963
heidsoft <heidsoft@sina.com>
2017-03-19 23:48:19 +0800

    Update 更新模块

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,29 +1,110 @@
-import { Component } from '@angular/core';
-import {Http,URLSearchParams } from '@angular/http';
-import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/map';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "my-app",
+  templateUrl: "./app.component.html",
 })
+export class AppComponent  {
+  name = "world";
+  closeMessage: string = "";
+  indeterminateState: boolean = true;
 
-@Injectable()
-export class AppComponent {
-  title = 'app works!';
-  constructor(private myjsonp: Http) {
-    let wikiUrl = 'http://10.0.2.209:8009/api/login';
-    let params = new URLSearchParams();
-    params.set('username', 'admin'); // the user's search value
-    params.set('password', 'zabbix');
-    console.log(myjsonp);
-    myjsonp.post(wikiUrl, { search: params }).subscribe((res) => {
-       console.log("response......start");
-       console.log(res);
-       console.log("response......end");
-    });
-          
-    
+  onClose(): void {
+    this.closeMessage = "The alert has been closed";
   }
+
+  rootDirectory: any[] = [
+    {
+      name: "Applications",
+      icon: "folder",
+      expanded: true,
+      files: [
+        {
+          icon: "calendar",
+          name: "Calendar",
+          active: true
+        },
+        {
+          icon: "line-chart",
+          name: "Charts",
+          active: false
+        },
+        {
+          icon: "dashboard",
+          name: "Dashboard",
+          active: false
+        },
+        {
+          icon: "map",
+          name: "Maps",
+          active: false
+        },
+      ]
+    },
+    {
+      name: "Files",
+      icon: "folder",
+      expanded: false,
+      files: [
+        {
+          icon: "file",
+          name: "Cover Letter.doc",
+          active: false
+        },
+      ]
+    },
+    {
+      name: "Images",
+      icon: "folder",
+      expanded: false,
+      files: [
+        {
+          icon: "image",
+          name: "Screenshot.png",
+          active: false
+        },
+      ]
+    }
+  ];
+
+  users: any[];
+  selected: any[] = [];
+  toAdd: any[] = [];
+  toDelete: any[] = [];
+  toEdit: any;
+
+  constructor() {
+    this.users = [
+      { name: "alice" },
+      { name: "bob" },
+      { name: "carol" },
+    ];
+  }
+
+  onDelete() {
+    this.toDelete = this.selected.slice();
+    return false;
+  }
+
+  onEdit() {
+    this.toEdit = this.selected[0];
+    return false;
+  }
+
+  onAdd() {
+    this.toAdd = this.selected.slice();
+  }
+
+  //打开文件
+  openFile(directoryName: string, fileName: string) {
+    console.log("directoryName",directoryName);
+    console.log("fileName",fileName);
+  }
+
+
 }
+
+
+
+
+
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule,JsonpModule } from '@angular/http';
 import { ClarityModule } from 'clarity-angular';
 import { AppComponent } from './app.component';
+import { TreeListComponent } from './tree.component';
 import { MaterialModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  { path: 'tree', component: TreeListComponent },
+  // { path: '',   redirectTo: '/tree', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TreeListComponent
   ],
   imports: [
     BrowserModule,
     ClarityModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
     FormsModule,
     HttpModule,
     JsonpModule,
