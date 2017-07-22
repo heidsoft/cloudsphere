6522ca6af2f3443127081aa2137355cd69bc23fc
heidsoft <heidsoft@qq.com>
2017-07-22 21:07:32 +0800

    Update 重新构建自动发现服务器

--- ngcloud-server/src/main/java/com/heidsoft/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.heidsoft.repository;
-
-import com.heidsoft.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
