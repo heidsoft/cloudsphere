774ae6bd79e4272741c4fc6da65ff8044472f15c
Bin Liu <bin.liu@magicwindow.cn>
2017-11-25 23:56:50 +0800

    Update 修改配置

--- ngcloud-registry/src/main/java/com/heidsoft/NgRegistryServer.java
+++ ngcloud-registry/src/main/java/com/heidsoft/NgRegistryServer.java
@@ -6,17 +6,21 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * <p>服务注册服务中心</p>
+ *
+ * @author heidsoft
+ */
 @SpringBootApplication
 @EnableEurekaServer
-public class NgCloudApplicationServer extends SpringBootServletInitializer {
+public class NgRegistryServer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(NgCloudApplicationServer.class);
+		return application.sources(NgRegistryServer.class);
 	}
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(NgCloudApplicationServer.class, args);
+		SpringApplication.run(NgRegistryServer.class, args);
 	}
 }
--- ngcloud-session/src/main/java/com/heidsoft/Application.java
+++ ngcloud-session/src/main/java/com/heidsoft/Application.java
@@ -1,4 +1,4 @@
-package com.airkisser;
+package com.heidsoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
