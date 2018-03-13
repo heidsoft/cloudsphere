898cbd6218f6197abc7299a6a6fcc50e222e2368
Bin Liu <bin.liu@magicwindow.cn>
2018-03-13 22:39:32 +0800

    Update 加入flume agent启动

---
+++ ngcloud-flume/readme.md
@@ -0,0 +1,3 @@
+# start flume agent
+
+ bin/flume-ng agent --conf conf --conf-file conf/avro_example.conf --name a1 -Dflume.root.logger=INFO,console
--- ngcloud-flume/src/main/java/MyApp.java
+++ ngcloud-flume/src/main/java/MyApp.java
@@ -9,7 +9,7 @@ public class MyApp {
   public static void main(String[] args) {
     MyRpcClientFacade client = new MyRpcClientFacade();
     // Initialize client with the remote Flume agent's host and port
-    client.init("host.example.org", 41414);
+    client.init("0.0.0.0", 41414);
 
     // Send 10 events to the remote Flume agent. That agent should be
     // configured to listen with an AvroSource.
