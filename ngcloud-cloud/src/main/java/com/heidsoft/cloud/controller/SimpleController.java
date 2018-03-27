195e32652ebbb236cbe1341bbacfd04e7ae54f75
Bin Liu <bin.liu@magicwindow.cn>
2018-03-27 19:16:36 +0800

    Update 调通界面

--- ngcloud-cloud/src/main/java/com/heidsoft/cloud/controller/SimpleController.java
+++ ngcloud-cloud/src/main/java/com/heidsoft/cloud/controller/SimpleController.java
@@ -1,7 +1,5 @@
 package com.heidsoft.cloud.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
