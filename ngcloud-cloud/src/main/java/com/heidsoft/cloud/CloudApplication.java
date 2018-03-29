bd707925b7d36b48f379bab07fc23d079d8ec7c3
Bin Liu <bin.liu@magicwindow.cn>
2018-03-29 15:38:41 +0800

    Update 增加链接es

--- ngcloud-cloud/src/main/java/com/heidsoft/cloud/CloudApplication.java
+++ ngcloud-cloud/src/main/java/com/heidsoft/cloud/CloudApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CloudApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CloudApplication.class, args);
     }
 }
