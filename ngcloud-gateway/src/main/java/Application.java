3b5188b59877372438a1632d897fb12b0b44174d
Bin Liu <bin.liu@magicwindow.cn>
2018-04-23 20:30:00 +0800

    Update 增加链接es

--- ngcloud-gateway/src/main/java/Application.java
+++ ngcloud-gateway/src/main/java/Application.java
@@ -1,13 +1,15 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
 
-@EnableZuulProxy
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class Application {
-
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
 
+    SpringApplication.run(Application.class, args);
+
+  }
 }
