c67e567789e7b375bafe18913e3d9432051ea5d8
Bin Liu <bin.liu@magicwindow.cn>
2018-04-24 17:31:42 +0800

    Update

--- ngcloud-eureka/src/main/java/com/heidsoft/EurekaServer.java
+++ ngcloud-eureka/src/main/java/com/heidsoft/EurekaServer.java
@@ -1,7 +1,7 @@
 package com.heidsoft;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -13,6 +13,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServer {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder().run(args);
+		SpringApplication.run(EurekaServer.class, args);
 	}
 }
