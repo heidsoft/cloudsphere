8dbf89ca751f5a6b4f4009f5928b10ff91955a7a
Bin Liu <bin.liu@magicwindow.cn>
2018-07-22 16:01:46 +0800

    Updsate

--- ngcloud-frontend/deploy.sh
+++ ngcloud-frontend/deploy.sh
@@ -1,1 +1,3 @@
 scp -r dist/* root@47.97.196.208:/var/www/ngtoken
+
+scp -r /Users/heidsoft/research/ngcloud/ngcloud-frontend/sample/zeedapp/zeedapp/* root@47.97.196.208:/var/www/ngtoken/ngcloud-frontend/
