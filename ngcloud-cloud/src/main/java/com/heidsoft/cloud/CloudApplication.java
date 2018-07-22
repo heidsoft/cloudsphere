f835f045d40c4a5b3d70607de2a176386122e409
Bin Liu <bin.liu@magicwindow.cn>
2018-07-22 15:07:07 +0800

    Update 测试springboot

--- ngcloud-cloud/src/main/java/com/heidsoft/cloud/CloudApplication.java
+++ ngcloud-cloud/src/main/java/com/heidsoft/cloud/CloudApplication.java
@@ -1,6 +1,6 @@
 package com.heidsoft.cloud;
 
-import org.springframework.boot.SpringApplication;
+import  org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
