8bf49e12c500c310788e988f78e9d5d2f8cc7d8a
Bin Liu <bin.liu@magicwindow.cn>
2018-10-14 19:05:10 +0800

    update 修改config

--- ngcloud-configserver/src/main/java/com/heidsoft/ConfigServerApplication.java
+++ ngcloud-configserver/src/main/java/com/heidsoft/ConfigServerApplication.java
@@ -1,3 +1,5 @@
+package com.heidsoft;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
